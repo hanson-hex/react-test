c17c9f0dd2c12b6e15d5fbfc570f9c98babc8a74
RemyHe <hex192039@163.com>
2020-07-16 19:11:19 +0800

    counter 提交

--- src/Components/FcPage.js
+++ src/Components/FcPage.js
@@ -1,4 +1,4 @@
-import React, { useState, Component, useRef } from 'react';
+import React, { useState, Component, useRef, useEffect } from 'react';
 
 class Counter2 extends Component {
     constructor (props) {
@@ -18,10 +18,11 @@ class Counter2 extends Component {
     };
 
     render() {
+        console.log('counter2');
         return (
             <div>
                 <h1>counter2</h1>
-                <p>you click {this.state.count} time</p>
+                <p>you click {this.state.count} times</p>
                 <button onClick={this.log}>click me</button>
             </div>
         )
@@ -38,6 +39,7 @@ function Counter () {
             console.log(count);
         }, 3000);
     };
+    console.log('counter')
     return (
         <div>
             <h1>counter1</h1>
@@ -65,6 +67,58 @@ function Counter3 () {
     );
 };
 
+function Counter4 () {
+    const [count, setCount] = useState(0);
+    const currentCount = useRef(count);
+
+    useEffect(() => {
+        currentCount.current = count;
+    });
+
+    const log = () => {
+        setCount(count + 1);
+        setTimeout(() => {
+            console.log(currentCount.current);
+        }, 3000);
+    };
+    return (
+        <div>
+            <h1>counter4</h1>
+            <p>you click {count} times</p>
+            <button onClick={log}>click</button>
+        </div>
+    );
+};
+
+function Counter5 () {
+    const [count, setCount] = useState(0);
+    const currentCount = UseCurrentValue(count);
+
+    const log = () => {
+        setCount(count + 1);
+        setTimeout(() => {
+            console.log(currentCount.current);
+        }, 3000);
+    };
+    return (
+        <div>
+            <h1>counter5</h1>
+            <p>you click {count} times</p>
+            <p>{currentCount.current}</p>
+            <button onClick={log}>click</button>
+        </div>
+    );
+};
+
+function UseCurrentValue(value) {
+    const ref = useRef(0);
+
+    useEffect(() => {
+        ref.current = value;
+    }, [value]);
+    return ref;
+};
+
 function FcPage () {
     return (
     <div>
@@ -72,6 +126,8 @@ function FcPage () {
         <Counter/>
         <Counter2></Counter2>
         <Counter3/>
+        <Counter4/>
+        <Counter5/>
     </div>
     );
 };
